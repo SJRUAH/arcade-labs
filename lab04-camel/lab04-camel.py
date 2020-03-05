d0ecfc724d015d4284601dd692edaff61601ca93
SamuelJiménezRodero <61016245+SJRUAH@users.noreply.github.com>
2020-03-05 20:39:27 +0100

    Update lab04-camel.py
    Casi terminado

--- lab04-camel/lab04-camel.py
+++ lab04-camel/lab04-camel.py
@@ -7,27 +7,53 @@ desert trek and out run the natives"""
 
 
 def main():
+    thirst = 0
+    miles_Travelled = 0
+    camel_tiredness = 0
+    milles_travelled_by_natives = -20
+    Drinks_available = 3
     print(Initial_Text)
     done = False
     print("What is your choice?")
     while not done:
-        print("""A. Drink from your canteen.
-            B. Ahead moderate speed.
-            C. Ahead full speed.
-            D. Stop for the night.
-            E. Status check.
-            Q. Quit.""")
+        print("""
+A. Drink from your canteen.
+B. Ahead moderate speed.
+C. Ahead full speed.
+D. Stop for the night.
+E. Status check.
+Q. Quit.""")
+        print("What is your choice?")
         Letra =input().upper()
+        AvanceNativos = random.randint(7, 14)
+        AvanceCamello = random.randint(10, 20)
+        AvanceCamelloModerado = random.randint(5,12)
+        Cansancio = random.randint(1,3)
         if Letra == "A":
-            pass
+            if Drinks_available <1:
+                    print("You dont have any water")
+            if Drinks_available >= 1:
+                thirst = 0
+                Drinks_available -= 1
         elif Letra == "B":
-            pass
+            miles_Travelled += AvanceCamelloModerado
+            milles_travelled_by_natives += AvanceNativos
+            thirst += 1
+            camel_tiredness += 1
         elif Letra == "C":
-            pass
+            miles_Travelled +=  AvanceCamello
+            print("Milles travelled:", miles_Travelled)
+            thirst += 1
+            camel_tiredness += Cansancio
+            milles_travelled_by_natives += AvanceNativos
         elif Letra == "D":
-            pass
+            print("The camel is happy and full of energy!")
+            camel_tiredness = 0
+            milles_travelled_by_natives += AvanceNativos
         elif Letra == "E":
-            pass
+            print("Miles travelled:", miles_Travelled)
+            print("Drinks in canteen:", Drinks_available)
+            print("The natives are", miles_Travelled - milles_travelled_by_natives, "miles behind you")
         elif Letra == "Q":
             done = True
             print("Turning off the game")
