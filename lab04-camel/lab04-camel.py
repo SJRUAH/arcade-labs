991a1ae16633a9d265e8e01b3f3a093824c6269b
SamuelJiménezRodero <61016245+SJRUAH@users.noreply.github.com>
2020-03-05 19:31:45 +0100

    Update lab04-camel.py
    Comienzo

---
+++ lab04-camel/lab04-camel.py
@@ -0,0 +1,24 @@
+import random
+
+Initial_Text = """Welcome to Camel!
+You have stolen a camel to make your way across the great Mobi desert.
+The natives want their camel back and are chasing you down! Survive your
+desert trek and out run the natives"""
+
+
+def main():
+    print(Initial_Text)
+    done = False
+    print("What is your choice?")
+    while not done:
+        print("""A. Drink from your canteen.
+                 B. Ahead moderate speed.
+                 C. Ahead full speed.
+                 D. Stop for the night.
+                 E. Status check.
+                 Q. Quit.""")
+        Letra = input()
+
+
+
+main()
