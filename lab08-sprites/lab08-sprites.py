d228b31e7ef9e626f43a6cb110ad50d3192a7bdc
SamuelJiménezRodero <61016245+SJRUAH@users.noreply.github.com>
2020-03-26 16:45:44 +0100

    Practica 7 y 8

--- lab08-sprites/lab08-sprites.py
+++ lab08-sprites/lab08-sprites.py
@@ -64,7 +64,7 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
 
         # Set up the player
-        self.player_sprite = Player(r"C:\Users\hidde\Documents\GitHub\arcade-labs\lab08-sprites\Chicorita-terminado.JPEG", SPRITE_SCALING)
+        self.player_sprite = Player(r"C:\Users\hidde\Documents\GitHub\arcade-labs\lab08-sprites\Chicooo.png", SPRITE_SCALING)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
